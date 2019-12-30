38b9cfbd7ea600420cb82088a65a879cb6bf1d5a
Talha 2019 <sayyed_talha@hotmail.com>
2019-12-31 02:46:23 +0500

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rentalapp';
+
+  componentTitle = "I am app component from component.ts file"
+
+  clicked(){
+    alert("I am clicked")
+  }
 }
